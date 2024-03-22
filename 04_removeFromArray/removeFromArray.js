af98fb909a9b9df7ce53e2a6ce66745765cf6670
cnpadua <49352065+cnpadua@users.noreply.github.com>
2024-03-22 16:12:55 -0700

    Complete Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let result = [];
+    for (let i=0; i<arr.length; i++){
+        let unique = true;
 
+        for (let j=1; j<arguments.length; j++){
+            if (arr[i] === arguments[j]){
+                unique = false;
+            }
+        }
+
+        if (unique) {
+            result.push(arr[i]);
+        }
+
+    }
+    return result;
 };
 
+// console.log(removeFromArray([1,2,3,4],3));
+// console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
