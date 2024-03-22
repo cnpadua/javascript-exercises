929416610fc3c30e4268f625c7698cae2fb86c34
cnpadua <49352065+cnpadua@users.noreply.github.com>
2024-03-22 16:20:31 -0700

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
 
+    if (first < 0 || last < 0){
+        return "ERROR";
+    }
+
+    if (typeof(first) != "number" || typeof(last) != "number"){
+        return "ERROR";
+    }
+
+    let result = 0;
+
+    // swap first and last if first > last
+    if (first > last){
+        let temp = first;
+        first = last;
+        last = temp;
+    }
+
+    for (let i=first; i<last; i++){
+        result = result + i;
+    }
+    return result + last;
 };
 
 // Do not edit below this line
