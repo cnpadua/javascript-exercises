c305da6acd058079b3fd840fcdc20d39ef670152
cnpadua <49352065+cnpadua@users.noreply.github.com>
2024-03-22 17:00:08 -0700

    Complete Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celciusTemp = (temp - 32) * (5/9);
+
+  if (Number.isInteger(celciusTemp)){
+    return +celciusTemp;
+  } else {
+    return +celciusTemp.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheitTemp = temp * (9/5) + 32;
+
+  if (Number.isInteger(fahrenheitTemp)){
+    return +fahrenheitTemp;
+  } else {
+    return +fahrenheitTemp.toFixed(1);
+  }
 };
 
 // Do not edit below this line
