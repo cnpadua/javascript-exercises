6b9537337d6c31a864b3dbdf1451f9906a65ac3c
Camille <49352065+cnpadua@users.noreply.github.com>
2024-05-27 15:23:09 -0700

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(a, b) {
+  return a+b;
+};
+
+const subtract = function(a,b) {
+  return a-b;
+};
+
+const sum = function(arr) {
+  if (arr.length === 0){
+    return 0;
+  } else {
+    return arr.reduce((total, curr) => {
+      return total + curr
+    }, 0);
+  }
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(arr) {
+  return arr.reduce((total,curr)=>{
+    return total * curr;
+  }, 1);
 };
 
-const sum = function() {
-	
+const power = function(a, b) {
+  return a**b;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0){
+    return 1;
+  } else {
+    return a * factorial(a-1);
+  }
 };
 
 // Do not edit below this line
