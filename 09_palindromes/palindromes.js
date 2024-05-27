35e28995a8b3db666a77a15842afc07a405eb2c5
Camille <49352065+cnpadua@users.noreply.github.com>
2024-05-27 16:26:22 -0700

    Completed 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    phrase = phrase.toLowerCase();
+    // console.log(phrase);
 
+    let letters_only = [...phrase.matchAll(/\w/g)];
+    // console.log(letters_only);
+
+    phrase = letters_only.join("");
+    let reversed_phrase = letters_only.reverse().join("");
+
+    // console.log(phrase);
+    // console.log(reversed_phrase);
+    return phrase === reversed_phrase;
+    
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
