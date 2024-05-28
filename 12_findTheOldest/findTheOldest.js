ab2877157cff2865febf22f366842f1a208a0ba1
Camille <49352065+cnpadua@users.noreply.github.com>
2024-05-27 21:33:02 -0700

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function getAge(person){
+    if (person.yearOfDeath == undefined){
+        let today = new Date();
+        return today.getFullYear() - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
 
+const findTheOldest = function(people) {
+    people.sort((a,b)=> getAge(b) - getAge(a));
+    return people[0];
 };
 
 // Do not edit below this line
